1a11b5cd76586508919f0eedff41434f54383bec
Victory <git@dfhu.org>
2015-09-10 19:08:43 -0400

    cleanup with a setUp

--- app/src/androidTest/java/org/dfhu/androidunittestingexamples/FullScreenActivityTest.java
+++ app/src/androidTest/java/org/dfhu/androidunittestingexamples/FullScreenActivityTest.java
@@ -4,18 +4,24 @@ package org.dfhu.androidunittestingexamples;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
 
-import junit.framework.TestCase;
-
 public class FullScreenActivityTest extends ActivityInstrumentationTestCase2<FullscreenActivity> {
+
+    public FullscreenActivity mActivity;
+    public Button mGoButton;
 
     public FullScreenActivityTest() {
         super(FullscreenActivity.class);
     }
 
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = getActivity();
+        mGoButton = (Button) mActivity.findViewById(R.id.goButton);
+    }
+
     public void testDummyFunction ()
     {
-        FullscreenActivity activity = getActivity();
-        Button button = (Button) activity.findViewById(R.id.goButton);
-        assertTrue(button != null);
+        assertTrue(mGoButton != null);
     }
 }
